d4b9f9ac703651dae145fbe98aed86ee5548119d
DuTran-7 <grayhathacker1997@gmail.com>
2020-11-21 17:33:13 +0700

    add Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FE';
+  title = 'Book Here';
+  searchshow = false;
+  constructor() {
+    window.addEventListener("click", () => {
+      this.searchshow == true;
+    });
+  }
+  showSearchBox(): void {
+      console.log(this.searchshow);
+    this.searchshow = !this.searchshow;
+    
+  }
+  onClick(event: Event): void {
+    event.stopPropagation();
+    this.searchshow = !this.searchshow;
+  }
+  
 }
+  
